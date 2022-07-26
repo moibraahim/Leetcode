5791c765cebd7ff73eab31115baedad19c5d0870
Mohamed Ibrahim <60974619+moibraahim@users.noreply.github.com>
2022-07-26 17:28:59 +0200

    Time: 145 ms (27.08%), Space: 48.3 MB (77.65%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int goal = n-1;
+        
+        for (int i=n-2; i>=0; i--)
+        {
+            if (nums[i] + i >= goal)
+            {
+                goal = i;
+                
+            }
+          
+        }
+        if (goal == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
