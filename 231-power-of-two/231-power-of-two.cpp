8ebe22204cc0928b2086a112a4d99038aba662ba
Mohamed Ibrahim <60974619+moibraahim@users.noreply.github.com>
2022-08-26 22:03:42 +0200

    Time: 3 ms (59.97%), Space: 6.1 MB (26.88%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+     
+       if(n==0)
+       {
+           return false;
+       }
+        
+        return floor(log2(n)) == ceil(log2(n))?true:false;
+      
+        
+    }
+};
