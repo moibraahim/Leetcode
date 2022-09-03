cff606e2e1901837f5c7cebbeaa4f9ea483b0cba
Mohamed Ibrahim <60974619+moibraahim@users.noreply.github.com>
2022-09-03 16:52:47 +0200

    Time: 8 ms (26.75%), Space: 16.1 MB (20.64%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    
+       vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;       
+        helper(0, nums, temp);
+        return result;
+    }
+    
+    void helper(int i, vector<int> &nums, vector<int> temp)
+    {
+        if(i>=nums.size())
+        {
+            result.push_back(temp);
+            return;
+        }
+        
+        helper(i+1,nums, temp);
+        temp.push_back(nums[i]);
+        helper(i+1, nums, temp);
+    }
+    
+ 
+};
