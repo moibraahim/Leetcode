5802ce19854182008586108037843bb7af073420
Mohamed Ibrahim <60974619+moibraahim@users.noreply.github.com>
2022-09-26 01:36:34 +0200

    Time: 357 ms (7.94%), Space: 93.3 MB (88.95%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int MaxProfit = 0;
+        int Profit = 0;
         int L = 0;
         int R = 1;
-        int Profit = 0;
-        int MaxProfit = 0;
         
         while (R < prices.size())
         {
-            if (prices[L] < prices[R])
-            {
-                Profit = prices[R] - prices[L];
-                MaxProfit = max(MaxProfit,Profit);
-                R++;
-                
-            }
-            else
+            if (prices[R]  < prices[L])
             {
                 L = R;
+            }
+            else 
+            {
+                
+                Profit = prices[R] - prices[L];
+                MaxProfit = max(Profit,MaxProfit);
                 R++;
             }
+          
+        
+           
         }
         
         return MaxProfit;
+        
+        
         
     }
 };
