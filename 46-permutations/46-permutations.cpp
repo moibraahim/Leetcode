deebefb2359c11cc8687a4a4b8f8d0edab68ad4e
Mohamed Ibrahim <60974619+moibraahim@users.noreply.github.com>
2022-09-29 10:42:31 +0200

    Time: 3 ms (83.77%), Space: 7.7 MB (55.54%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> temp;
+        vector<int> map(nums.size(),0);
+      
+        vector<vector<int>> result;
+        
+        Helper(nums,temp,map,result);
+        return result;
+        
+        
+    }
+    
+    void Helper(vector<int>& nums, vector<int>& temp, vector<int>& map, vector<vector<int>>& result )
+    {
+        if (temp.size() == nums.size())
+        {
+            result.push_back(temp);
+            return;
+        }
+        
+        // choose start point
+        for (int i = 0; i<nums.size(); i++)
+        {
+            if (!map[i])
+            {
+                temp.push_back(nums[i]);
+                map[i] = 1;
+                Helper(nums,temp,map,result);
+                map[i] = 0;
+                temp.pop_back();
+                    
+            }
+            
+         
+        }
+    }
+};
